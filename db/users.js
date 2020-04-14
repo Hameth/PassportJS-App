7e9d2a144efe62afb44ab691f5fef9b9ca596815
AnaHdez98 <42677040+AnaHdez98@users.noreply.github.com>
2020-04-13 22:21:11 -0500

    users.js
    Agregue mas usuario :)

--- db/users.js
+++ db/users.js
@@ -1,7 +1,11 @@
 var records = [
     { id: 1, username: 'Momo ', token: '123456789', displayName: 'Moo', emails: [{ value: 'momo@hotmail.com' }] }
     , { id: 2, username: 'Momillito', token: 'abcdefghi', displayName: 'Pinguino', emails: [{ value: 'momillito@icloud.com' }] }
-    
+    , { id: 3, username: 'Cindy', token: '11223344', displayName: 'Cimiheja', emails: [{ value: 'cimiheja@gmail.com' }] }
+    , { id: 4, username: 'Jennifer', token: '77885544', displayName: 'Jennheja', emails: [{ value: 'jennheja@hotmail.com' }] }
+    , { id: 5, username: 'Liz', token: 'lulapaluza', displayName: 'lizzie', emails: [{ value: 'lizzio@icloud.com' }] }
+    , { id: 6, username: 'Laura', token: 'manilla', displayName: 'lau', emails: [{ value: 'laurita@gmail.com' }] }
+    , { id: 7, username: 'Mapache', token: '98745578', displayName: 'mapu', emails: [{ value: 'mapachetote@hotmail.com' }] }
 ];
 
 exports.findByToken = function (token, cb) {
