f8d8117c9efe49a0ed68aafd52f2e067f0fadb23
AnaHdez98 <42677040+AnaHdez98@users.noreply.github.com>
2020-04-13 22:15:05 -0500

    index.js
    index.js :100:

--- db/users.js
+++ db/users.js
@@ -1,6 +1,7 @@
 var records = [
     { id: 1, username: 'Momo ', token: '123456789', displayName: 'Moo', emails: [{ value: 'momo@hotmail.com' }] }
     , { id: 2, username: 'Momillito', token: 'abcdefghi', displayName: 'Pinguino', emails: [{ value: 'momillito@icloud.com' }] }
+    
 ];
 
 exports.findByToken = function (token, cb) {
